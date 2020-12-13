33453b2575e9b192e98ebb86c3fea919cb2a4f0f
Rohan Singh <40713164+tagrohan@users.noreply.github.com>
2020-12-14 00:08:21 +0530

    Update and rename Bottom sheet dialog to Bottom.java

---
+++ Bottom.java
@@ -0,0 +1,26 @@
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.View;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    public static BottomSheetFragment newInstance() {
+        BottomSheetFragment fragment = new BottomSheetFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        View contentView = View.inflate(getContext(), R.layout.fragment_bottomsheet, null);
+        dialog.setContentView(contentView);
+        ((View) contentView.getParent()).setBackgroundColor(getResources().getColor(android.R.color.transparent));
+    }
+
+}
